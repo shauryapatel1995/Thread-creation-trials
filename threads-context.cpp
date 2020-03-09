1a927c512c06b9505f25082dcaca62a8deb416b1
shaurya <shauryakamle@umass.edu>
2020-03-09 19:21:13 -0400

    Thread exit implementation

--- threads-context.cpp
+++ threads-context.cpp
@@ -80,6 +80,7 @@ void THREAD_YIELD(int signum) {
 void init_scheduler() {
     cout << "Initializing scheduler\n";
 
+    // Create scheduler context
     ucontext_t uc;
     getcontext(&sched);
     sched.uc_stack.ss_sp = malloc(1024*64);
@@ -89,71 +90,74 @@ void init_scheduler() {
     sched.uc_link = &main_context;
 
     
+    //Create signal handler 
     struct sigaction sa;
     // Create the new stack
     stack.ss_flags = 0;
     stack.ss_size = 1024*64;
     stack.ss_sp = malloc( 1024*512 );
     cout << "Got stack!\n";
+
+    //Set up separate stack for signal handling
     sigaltstack( &stack, 0 );
 
     sa.sa_handler = &THREAD_YIELD;
     sa.sa_flags = SA_ONSTACK;
     sigemptyset( &sa.sa_mask );
     sigaction( SIGALRM, &sa, 0 );
-    struct itimerval timer; 
-
-    timer.it_value.tv_sec = 0;
-    timer.it_value.tv_usec = 50000;
-    timer.it_interval = timer.it_value;
     
-    setitimer(ITIMER_REAL, &timer, NULL);
+    
     initscheduler = 1;
     cout << "INITIALIZAION COMPLETE!\n";
 }
 
+thread_exit() {
+    // Remove thread from the queue. 
+    using_queue.lock();
+    cout << "\nRemoving from queue!\n";
+    threads.pop();
+    cout << "Queue size is: " << threads.size() << "\n";
+    using_queue.unlock();
+}
+
 void scheduler() {
 
-    //Scheduler just schedules the current top thread
-    //Changes the context back to the current process. 
-    // We need a context for this and then for the thread itself. 
-    //Current thread has to be initialized first!    
+    //Need to setup the current process
     using_queue.lock();
     current_thread = threads.front();
     cout << "Current thread set: "<< current_thread->thread_id << "\n";
     using_queue.unlock();    
     
+    //Initialize scheduler 
     if(!initscheduler) {
         init_scheduler();    
     }  
     
-    while(1) {
+
+    //Loop through scheduler
+    while(threads.size() != 0) {
             
-        cout << "Scheduler\n";
-        cout << "Scheduling process!\n";
         using_queue.lock();
         threads.pop();
         threads.push(current_thread);
         current_thread = threads.front();
         using_queue.unlock();
-        cout << "Changed context!\n";    
         cout << current_thread->thread_id;
         cout << "Current thread being scheduled is: " << current_thread->thread_id << "\n";
         
 
         cout << "Swapping context!";
-        swapcontext(&sched, &(current_thread->saved_context));            
-        cout << "WE ARE LEAVING BITCH!\n";
+        // Setup timer for preemption
+        struct itimerval timer; 
+        timer.it_value.tv_sec = 0;
+        timer.it_value.tv_usec = 50000;
+        //timer.it_interval = timer.it_value;
+    
+        setitimer(ITIMER_REAL, &timer, NULL);
+        swapcontext(&sched, &(current_thread->saved_context));   
+        thread_exit();
     }
 } 
-
-/*#define thread_yield() {\
-    //Store the context for the thread here
-    if(setjmp(current_thread->saved_context) == 0) {\
-        current_thread->context_changed = 1;\
-        longjmp(sched, 1);\
-    }\
-} */
 
 int thread_create(void *(*start_routine) (void *), void* arg) {
 	Thread *thread = new Thread();
@@ -161,33 +165,35 @@ int thread_create(void *(*start_routine) (void *), void* arg) {
     thread->func = start_routine;
     thread->arg = arg;
     
-    //cout << "Initializing context!\n";
+    
     getcontext(&(thread->saved_context));
 
     thread->saved_context.uc_stack.ss_sp = malloc(1024*64);
     thread->saved_context.uc_stack.ss_size = sz;
     thread->saved_context.uc_stack.ss_flags = 0;
 
-    //cout << "Setting next link for thread!\n";
+    
 	thread->saved_context.uc_link = &sched;
     cout << "What are we passing!: " << arg << "\n";
     makecontext(&(thread->saved_context), start_routine,1, arg);
-    //cout << "Made thread context!\n";
-    // Make context here and make it point to sched. 
-
+    
     
     using_queue.lock();
     //Push to thread queue. 
     threads.push(thread);
     cout << "Queue size is: " << threads.size() << "\n";
     using_queue.unlock();
+    
+    // Start scheduler on a new thread
     if(threads.size() == 1 && scheduler_activated == 0) {
         
         cout << "Starting scheduler\n";
         scheduler_activated = 1;
         std::thread thread1(scheduler);
         thread1.detach();
+    
     } else {
+        
         //Notify scheduler thread about thread
         cout << "Is this blocking?\n";
         new_thread.notify_all();
@@ -208,6 +214,11 @@ void some_function(int counter) {
         }
 
         cout << "Executing function!: " << counter << " Sum value is " << a << "\n";
+        if (a % 10 == 0) {
+            cout << "Returning!\n";
+            return;
+        }
+            
 
     }
     
